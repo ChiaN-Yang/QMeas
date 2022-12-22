ab14f7ac5078988daae07813e72855236cbe22e1
Chia En Yang <plusnsheep@gmail.com>
2022-12-22 23:12:38 +0800

    Refactor keithley2400

--- QMeas/instruments/smu/keithley2400.py
+++ QMeas/instruments/smu/keithley2400.py
@@ -1,43 +1,36 @@
-from utils import DriverInterface
+from ..instrument import InstrumentDriver
 from pymeasure.instruments.keithley import Keithley2400
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import strict_discrete_set
 
 
-class Driver(Keithley2400, DriverInterface):
+class Keithley2400(Keithley2400, InstrumentDriver):
     METHOD = ['Current', 'Current with Auto-range', 'Voltage']
 
     current_auto_range = Instrument.control(
-        ":SENS:CURR:RANG:AUTO?",":SENS:CURR:RANG:AUTO %d",
+        ":SENS:CURR:RANG:AUTO?", ":SENS:CURR:RANG:AUTO %d",
         """ A integer property that switches the measurement current
         range to Auto-range, which can take values between 1 or 0.
         """,
         validator=strict_discrete_set,
-        values=[0,1],
-        cast=int )
+        values=[0, 1],
+        cast=int)
 
     def __init__(self, visa_address):
         super().__init__(visa_address)
 
-    def performOpen(self):
-        """Perform the operation of opening the instrument connection"""
-        # Resets the instrument and clears the queue
-        # self.reset()
+    def perform_open(self):
         # Enables the front terminals for measurement, and disables the rear terminals.
         self.use_front_terminals()
         self.sample_continuously()
-        # Enables the source of voltage depending on the configuration of the instrument.
-        # self.enable_source()
         # First run helps to switch the Auto-range function
         self.first_run = True
 
-    def performClose(self):
-        """Perform the close instrument connection operation"""
+    def perform_close(self):
         self.stop_buffer()
         self.local_switch()
 
-    def performSetValue(self, option, value, sweepRate=0.0):
-        """Perform the Set Value instrument operation"""
+    def perform_set_value(self, option, value, sweepRate=0.0):
         if option == 'Current':
             if self.source_mode != 'current':
                 # Configures the instrument to apply a source current
@@ -53,8 +46,7 @@ class Driver(Keithley2400, DriverInterface):
 
         return value
 
-    def performGetValue(self, option, magnification):
-        """Perform the Get Value instrument operation"""
+    def perform_get_value(self, option, magnification):
         if option == 'Current':
             value = float(self.current[1]) * magnification
         elif option == 'Current with Auto-range':
@@ -70,8 +62,3 @@ class Driver(Keithley2400, DriverInterface):
     def local_switch(self):
         """ Disables remote mode and switch the system to local mode"""
         self.write(":SYSTEM:LOCAL")
-
-
-if __name__ == '__main__':
-    a = Driver('GPIB:24', 'K2400', 'Keithley2400')
-    a.performOpen
